3fa2b6f8cef1f409e74483887764b02339369d72
Rob McCoy <rollcast@gmail.com>
2017-11-06 14:39:24 -0500

    First commit

---
+++ server.js
@@ -0,0 +1,26 @@
+var express = require('express');
+var PORT = 3000;
+var app = express();
+
+//app.get('/', function(req, res){ //req and res are default parms in express
+//    res.send('Hello Express!');
+//});
+
+var middleware = require('./middleware.js');
+
+
+app.use(middleware.logger);
+//order defined is important
+// app.use calls application level middleware - for every request
+
+
+app.get('/about', middleware.requireAuthentication , function(req, res){ //req and res are default parms in express
+    res.send('About us!');
+});
+
+//console.log(__dirname);
+app.use(express.static( __dirname + '\\public'));
+
+app.listen(PORT, function(){
+    console.log('Express server started on poer '+ PORT +'!');
+});
