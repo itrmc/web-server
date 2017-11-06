b074697865ded5e3e1ed3663a79e9f269f2aef54
Rob McCoy <rollcast@gmail.com>
2017-11-06 15:56:35 -0500

    Changing to slash

--- server.js
+++ server.js
@@ -19,7 +19,7 @@ app.get('/about', middleware.requireAuthentication , function(req, res){ //req a
 });
 
 //console.log(__dirname);
-app.use(express.static( __dirname + '\\public'));
+app.use(express.static( __dirname + '/public'));
 
 app.listen(PORT, function(){
     console.log('Express server started on poer '+ PORT +'!');
